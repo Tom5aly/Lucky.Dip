919bfb5914bea6d1b788be78bb455723a0a5733b
Tom5aly <tom.aly@hotmail.com>
2019-01-16 17:37:09 +0000

    first commit

---
+++ js.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+  // function
+  $("#num0").click(function() {
+    // button id
+
+    $("#div0").fadeIn(3000); // show id
+  });
+});
+
+function my0() {
+  let luckyNum = Math.ceil(Math.random() * 49);
+  document.getElementById("div0").innerHTML = luckyNum;
+}
+
+//
+// secound line
+//
+
+$(document).ready(function() {
+  // function
+  $("#num1").click(function() {
+    // button id
+
+    $("#div1").fadeIn(3000); // show id
+  });
+});
+
+function my1() {
+  let luckyNum1 = Math.ceil(Math.random() * 20);
+  document.getElementById("div1").innerHTML = luckyNum1;
+}
+
+//
+// Third Line
+//
+$(document).ready(function() {
+  // function
+  $("#num2").click(function() {
+    // button id
+
+    $("#div2").fadeIn(3000); // show id
+  });
+});
+
+function my2() {
+  let luckyNum = Math.ceil(Math.random() * 30);
+  document.getElementById("div2").innerHTML = luckyNum;
+}
+
+//
+// line num 4
+//
+$(document).ready(function() {
+  // function
+  $("#num3").click(function() {
+    // button id
+
+    $("#div3").fadeIn(3000); // show id
+  });
+});
+
+function my3() {
+  let luckyNum = Math.ceil(Math.random() * 22);
+  document.getElementById("div3").innerHTML = luckyNum;
+}
+
+//
+// line num 5
+//
+$(document).ready(function() {
+  // function
+  $("#num4").click(function() {
+    // button id
+
+    $("#div4").fadeIn(3000); // show id
+  });
+});
+
+function my4() {
+  let luckyNum = Math.ceil(Math.random() * 45);
+  document.getElementById("div4").innerHTML = luckyNum;
+}
+
+//
+//line num 6
+//
+$(document).ready(function() {
+  // function
+  $("#num5").click(function() {
+    // button id
+
+    $("#div5").fadeIn(3000); // show id
+  });
+});
+
+function my5() {
+  let luckyNum = Math.ceil(Math.random() * 28);
+  document.getElementById("div5").innerHTML = luckyNum;
+}
